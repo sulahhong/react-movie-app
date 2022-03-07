20c5a208b5211a5057bdc91ad5a4a7050cbba4be
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-03-07 21:57:41 +0900

    css.moviedetaill

--- src/components/Moviedetail.js
+++ src/components/Moviedetail.js
@@ -1,6 +1,8 @@
 
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from './Moviedetail.module.css';
+import Navbar from "./Navbar";
 
 
 function Moviedetail(
@@ -9,37 +11,42 @@ function Moviedetail(
   console.log(yt)
 
     return (
-      <div>
+      
+      <div className={styles.container}>
+        <div className={styles.mainimg} >
         <img src={coverImg} alt={title} />
-        <h2>
+        </div>
+        <div className={styles.mainbox}>
+        <h2 className={styles.maindetail}>
           <Link to={`/movie/${id}`}>{title}</Link>
         </h2>
-        <div>
+        <div className={styles.mainbox}>
           <p>{rating && `rating: ${rating}`}</p>
           <p>{runtime && `runtime: ${runtime} (min)`}</p>
         </div>
-        <div>
+        <div className={styles.mainbox}>
         <b>{summary && `<summary>`}</b>
         <p>{summary}</p>
         </div>
-        <div>
+        <div className={styles.mainbox}>
           <b>{description_full && `<description>`}</b>
           <p>{description_full}</p>
         </div>
-        <div>
+        <div className={styles.mainbox}>
           <b>{genres && `<genres>`}</b>
           {movie.hasOwnProperty("genres") ? (<ul>{genres.map((g)=>(<li key={g}>{g}</li>))}</ul>) : null}
         <div>
           <p>{language && `language: ${language}`}</p>
-        </div>
-        <div>
           <p>{mpa_rating && `mpa rating: ${mpa_rating}`}</p>
         </div>
-        <div>
+          <div className={styles.ytlink}>
           <a href={`https://www.youtube.com/watch?v=${yt}`}>watch movie trailer</a>
         </div>
         </div>
+        <div className={styles.backbtn}>
         <Link to={"/"}>BACK</Link>
+        </div>
+        </div>
       </div>
     );
     }
