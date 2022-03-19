e2dc9f1c3998df97a20ebe5db687b6c80f2c84f9
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-03-19 21:50:07 +0900

    modal

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,24 +4,47 @@ import { Link } from "react-router-dom";
 import { useGlobalContext } from "./context";
 import styles from './Movie.module.css';
 
-function Movie({ id, coverImg, rating, runtime, description_full, title, summary, genres, movie}) {
+function Movie({
+  id,
+  coverImg,
+  rating,
+  runtime,
+  description_full,
+  title,
+  summary,
+  genres,
+  movie,
+}) {
   return (
-
-    
-      <div className={styles.bbox}>
-        <img src={coverImg} alt={title} />
-        <div className={styles.homeinfo}>
-          <h2 >
-            <Link to={`/movie/${id}`} className={styles.title}>{title}</Link>
-          </h2>
-          <p>{rating && `rating: ${rating}`}</p>
-          <p>{runtime && `runtime: ${runtime} (min)`}</p>
-          <p><b>{genres && `genres`}</b></p>
-            {movie.hasOwnProperty("genres") ? (<ul>{genres.map((g)=>(<li key={g}>{g}</li>))}</ul>) : null}
-        </div>
+    <div className={styles.bbox}>
+    <div className={styles.bbox2}>
+    <Link to={`/movie/${id}`} className={styles.title}>
+      <img src={coverImg} alt={title} />
+      <p>view detail</p></Link>
+    </div>
+      
+      <div className={styles.homeinfo}>
+        <h2>
+          <Link to={`/movie/${id}`} className={styles.title}>
+            {title}
+          </Link>
+        </h2>
+        <p>{rating && `rating: ${rating}`}</p>
+        <p>{runtime && `runtime: ${runtime} (min)`}</p>
+        {/* <p>
+          <b>{genres && `genres`}</b>
+        </p> */}
+        {movie.hasOwnProperty("genres") ? (
+          <ul>
+            {genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        ) : null}
       </div>
-    );
-    }
+    </div>
+  );
+}
 
 Movie.propTypes = {
     id:PropTypes.number.isRequired,
--- src/components/Moviedetail.js
+++ src/components/Moviedetail.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Moviedetail.module.css";
 import { RiArrowGoBackFill } from "react-icons/ri";
+import { useState } from "react";
 
 function Moviedetail({
   id,
@@ -17,44 +18,83 @@ function Moviedetail({
   mpa_rating,
   movie,
 }) {
-  console.log(yt);
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  // if (language == 'en') {
+  //   return language = 'English';
+  // } else {
+  //   return language;
+  // }
+
+  switch(language) {
+    case 'ko' : language = 'Korean';
+    break;
+    case 'en' : language = 'English';
+    break;
+  }
+  
 
   return (
+    <section>
     <div className={styles.container}>
-      <img src={coverImg} alt={title} />
-        <div className={styles.info}>
-          <h1>
-            <Link to={`/movie/${id}`}>{title}</Link>
-          </h1>
-            <p>{rating && `rating: ${rating}`}</p>
-            <p>{runtime && `runtime: ${runtime} (min)`}</p>
-            <p>{language && `language: ${language}`}</p>
-            <p>{mpa_rating && `mpa rating: ${mpa_rating}`}</p>
-            <p><b> genres : </b>
-            {movie.hasOwnProperty("genres") ? (
-              <ul>
-                {genres.map((g) => (
-                  <li key={g}>{g}</li>
-                  ))}
-              </ul>
-            ) : null} </p>
-              <p><b>synopsis : </b>{description_full}</p>
-          <button className={styles.ytlink}>
-            <a href={`https://www.youtube.com/watch?v=${yt}`}>  
-                watch movie trailer
-            </a>
+      <img className={styles.detailimg} src={coverImg} alt={title} />
+      <div className={styles.info}>
+        <h1>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h1>
+        <p>{rating && `rating: ${rating}`}</p>
+        <p>{runtime && `runtime: ${runtime} (min)`}</p>
+        <p>{language && `language: ${language}`}</p>
+        <p>{mpa_rating && `mpa rating: ${mpa_rating}`}</p>
+        <p>
+          <b> genres : </b>
+          {movie.hasOwnProperty("genres") ? (
+            <ul>
+              {genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          ) : null}{" "}
+        </p>
+
+        <button onClick={openModal}>modal</button>
+        <button className={styles.ytlink}>
+          <a href={`https://www.youtube.com/watch?v=${yt}`}>
+            watch movie trailer
+          </a>
+        </button>
+        <Link to={"/"}>
+          <button className={styles.backbtn}>
+            <RiArrowGoBackFill className={styles.backbtnicon} />
+            BACK
           </button>
-          <Link to={"/"}>
-            <button className={styles.backbtn}>
-              <RiArrowGoBackFill className={styles.backbtnicon} />
-              BACK
-            </button>
-            </Link>
-          </div>
-       </div>
+        </Link>
+      </div>
+      
+    </div>
     
+    {modal ? (
+      <p className={modal ? `${styles.open} ${styles.modalbg}`  : styles.close}>
+        <b>synopsis : </b>
+        {description_full}
+        <button className={styles.closebtn} onClick={closeModal}>
+          X
+        </button>
+      </p>
+      ) : null}
+    
+    </section>
   );
 }
+          // <div className={styles.modalcontainer}>
 
 Moviedetail.propTypes = {
   id: PropTypes.number.isRequired,
